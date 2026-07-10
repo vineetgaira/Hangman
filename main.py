8b0c54ea0457c755cc49c65eea4267e3decbc940
Vineet Gaira <vineetgaira1729@gmail.com>
2026-07-10 11:49:38 +0530

    Finish main.py

--- main.py
+++ main.py
@@ -1,9 +1,48 @@
 from game import start_game, get_guess, update_game, check_win, check_lose, play_again
-from display import welcome, show_word, show_wrong_letters, show_hangman,show_result
+from display import welcome, show_word, show_wrong_letters, show_hangman,show_result,clear_screen
 
 def main():
+    clear_screen()
+    welcome()
 
+    playing = True
+    while playing:
     
+        secret_word, guessed_letters, lives = start_game()
+        won = False
+        game_over =False
+        clear_screen()
+        
 
+        while not game_over:
+            clear_screen()
+            show_word(secret_word,guessed_letters)
+            show_wrong_letters(guessed_letters, secret_word)
+            show_hangman(lives)
+
+            status=None
+            while status != "ok":
+                status, guess = get_guess(guessed_letters)
+                if status == "invalid":
+                    print("Invalid input! Please enter a single character.")
+                elif status == "repeat":
+                    print(f"You have already guessed the letter {guess}")
+            
+            guessed_letters,lives=update_game(secret_word, guess, guessed_letters, lives)
+
+            if check_win(secret_word,guessed_letters):
+                game_over=True
+                won=True
+            elif check_lose(lives):
+                game_over=True
+                won = False
+        
+        show_result(won,secret_word)
+        playing= play_again()
+
+if __name__ == "__main__":
+    main()
+
+                
 
 
